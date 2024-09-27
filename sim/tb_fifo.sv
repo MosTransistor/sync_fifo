5081f588a1665c40bb4edc55532c07274d5b1e24
Jun <wangjun@Juns-MacBook-Pro.local>
2024-09-26 23:39:59 -0500

    Add first file, sync_fifo use single port ram to storage data

---
+++ sim/tb_fifo.sv
@@ -0,0 +1,78 @@
+`timescale 1ns/1ps
+
+module tb_fifo();
+
+    initial begin
+        $dumpfile("sync_fifo.vcd");
+        $dumpvars(0, tb_fifo);
+    end
+
+    logic           t_clk=0;
+    logic           t_rst_n=0;
+    
+    logic           t_wr=0;
+    logic  [31:0]   t_din=0;
+    logic           t_rd=0;
+    logic  [31:0]   t_dout;
+    logic  [3:0]    t_used_cnt;
+    logic           t_full;
+    logic           t_empty;
+    
+    int  ref_data[$];
+    int  imp_data[$];
+    logic t_rd_s;
+    integer fh, seed;
+
+    always #5 t_clk = ~t_clk;
+    
+    always @(posedge t_clk) begin
+        t_rd_s <= t_rd;
+        if (t_wr)  ref_data.push_back(t_din);
+        if (t_rd_s) imp_data.push_back(t_dout);
+    end
+
+    initial begin
+        fh = $fopen("random.txt", "r");
+        $fscanf(fh, "%d", seed);
+        $fclose(fh);
+
+        $display(" [INFO] simulation begin");
+        repeat (2) @(negedge t_clk);
+        t_rst_n = 1;
+
+        repeat (20000) begin
+            repeat (1) @(negedge t_clk);
+            case ({t_full, t_empty})
+                2'b00: begin t_wr = $urandom(seed); t_din = $urandom(seed); t_rd = $urandom(seed); end
+                2'b01: begin t_wr = $urandom(seed); t_din = $urandom(seed); t_rd = 1'b0; end
+                2'b10: begin t_wr = 1'b0; t_din = $urandom(seed); t_rd = $urandom(seed); end
+                2'b11: begin $display("\033[31m [FAIL] FIFO empty and full both equal to 1 \033[0m"); $finish; end
+            endcase
+        end
+
+        repeat (1) @(negedge t_clk);
+        while(!t_empty) begin
+            repeat (1) @(negedge t_clk);
+            t_wr = 1'b0; 
+            t_din = $urandom(seed); 
+            t_rd = $urandom(seed);
+        end
+
+        repeat (3) @(negedge t_clk);
+        $display(" [INFO] first data ref = %h, imp = %h",ref_data[0],imp_data[0]);
+        foreach (ref_data[i]) begin
+            if (ref_data[i] !== imp_data[i]) begin
+                $display("\033[31m [FAIL] last transfer = %h, ref_payload = %h, imp_payload = %h \033[0m",i-1,ref_data[i-1],imp_data[i-1]);
+                $display("\033[31m [FAIL] current transfer = %h, ref_payload = %h, imp_payload = %h \033[0m",i,ref_data[i],imp_data[i]);
+                $finish;
+            end
+        end
+        $display("\033[32m [PASS] simulation pass!!! \033[0m");
+        $finish;
+    end   
+
+    sync_fifo_spram #(.WIDTH(32), .DEPTH(15))  i_sync_fifo(.clk(t_clk), .rst_n(t_rst_n), 
+                                  .wr(t_wr), .din(t_din), .rd(t_rd), .dout(t_dout),
+                                  .used_cnt(t_used_cnt), .empty(t_empty), .full(t_full));
+
+endmodule
---
+++ src/sync_fifo_spram.v
@@ -0,0 +1,128 @@
+`define RD 1ns
+module sync_fifo_spram #(
+    parameter WIDTH = 8,
+    parameter DEPTH = 8
+) (
+    // global signal
+    input    wire                       clk,
+    input    wire                       rst_n,
+
+    // FIFO interface
+    input    wire                       wr,
+    input    wire  [WIDTH-1:0]          din,
+    input    wire                       rd,
+    output   wire  [WIDTH-1:0]          dout,
+    output   wire  [$clog2(DEPTH)-1:0]  used_cnt,
+    output   wire                       full,
+    output   wire                       empty
+);
+
+localparam AW = $clog2(DEPTH);
+localparam DEPTH_HALF = (DEPTH % 2 == 1) ? DEPTH / 2 + 1 : DEPTH / 2;
+
+wire  [AW-1:0]     used_cnt_c; 
+reg   [AW-1:0]     used_cnt_s; 
+
+wire  [AW-1:0]     waddr_c;
+reg   [AW-1:0]     waddr_r;
+wire  [AW-1:0]     raddr_c;
+reg   [AW-1:0]     raddr_r;
+
+wire               in2latch;
+wire               latch2ram;
+wire               in2ram;
+wire  [WIDTH-1:0]  data_latch_c;
+reg   [WIDTH-1:0]  data_latch_s;
+wire               latch_valid_c;
+reg                latch_valid_s;
+
+wire               even_ram_cs;
+wire               even_ram_wr;
+wire  [AW-2:0]     even_ram_addr;
+wire  [WIDTH-1:0]  even_ram_wdata;
+wire  [WIDTH-1:0]  even_ram_rdata;
+
+wire               odd_ram_cs;
+wire               odd_ram_wr;
+wire  [AW-2:0]     odd_ram_addr;
+wire  [WIDTH-1:0]  odd_ram_wdata;
+wire  [WIDTH-1:0]  odd_ram_rdata;
+
+
+// FIFO data counter
+assign used_cnt_c = wr ? (rd ? used_cnt_s : used_cnt_s + 1'b1) : (rd ? used_cnt_s - 1'b1 : used_cnt_s);
+
+//
+assign in2latch = wr & rd & ((waddr_r[0] == raddr_r[0]) |  latch_valid_s);
+assign data_latch_c = in2latch ? din : data_latch_s;
+assign latch_valid_c = in2latch ? 1'b1 : 1'b0;
+assign latch2ram = latch_valid_s;
+assign in2ram = wr & (~in2latch);
+
+// FIFO pointer 
+assign waddr_c = (latch2ram || in2ram) ? waddr_r + latch2ram + in2ram : waddr_r;
+assign raddr_c = rd ? raddr_r + 1'b1 : raddr_r;
+
+// RAM interface
+assign even_ram_cs = (rd & (~raddr_r[0])) | even_ram_wr;
+assign even_ram_wr = (latch2ram & (~waddr_r[0])) | (in2ram & ((~waddr_r[0]) | latch2ram));
+assign even_ram_addr = (!even_ram_wr) ? raddr_r[AW-1:1] :
+                       (latch2ram && in2ram && waddr_r[0]) ? waddr_r + 1'b1 >> 1 : waddr_r[AW-1:1];
+assign even_ram_wdata = (latch2ram && (!waddr_r[0])) ? data_latch_s : din;
+
+assign odd_ram_cs = (rd & raddr_r[0]) | odd_ram_wr;
+assign odd_ram_wr = (latch2ram & waddr_r[0]) | (in2ram & (waddr_r[0] | latch2ram));
+assign odd_ram_addr = (!odd_ram_wr) ? raddr_r[AW-1:1] : 
+                      (latch2ram && in2ram && (!waddr_r[0])) ? waddr_r + 1'b1 >> 1 : waddr_r[AW-1:1];
+assign odd_ram_wdata = (latch2ram && waddr_r[0]) ? data_latch_s : din;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        used_cnt_s    <= #`RD {AW{1'b0}};
+        waddr_r       <= #`RD {AW{1'b0}};
+        raddr_r       <= #`RD {AW{1'b0}};
+        latch_valid_s <= #`RD 1'b0;
+        data_latch_s  <= #`RD {WIDTH{1'b0}};
+    end
+    else begin
+        used_cnt_s    <= #`RD used_cnt_c;
+        waddr_r       <= #`RD waddr_c;
+        raddr_r       <= #`RD raddr_c;
+        latch_valid_s <= #`RD latch_valid_c;
+        data_latch_s  <= #`RD data_latch_c;
+    end
+end
+
+// FIFO output
+assign dout  = raddr_r[0] ? even_ram_rdata : odd_ram_rdata;
+assign full  = (used_cnt_s == DEPTH);
+assign empty = (used_cnt_s == 0);
+assign used_cnt = used_cnt_s;
+
+single_ram_model #(
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH_HALF)
+) 
+odd_ram(
+    .clk(clk),
+    .cs(odd_ram_cs),
+    .wr(odd_ram_wr),
+    .addr(odd_ram_addr),
+    .wdata(odd_ram_wdata),
+    .rdata(odd_ram_rdata)
+);
+
+single_ram_model #(
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH_HALF)
+) 
+even_ram(
+    .clk(clk),
+    .cs(even_ram_cs),
+    .wr(even_ram_wr),
+    .addr(even_ram_addr),
+    .wdata(even_ram_wdata),
+    .rdata(even_ram_rdata)
+);
+
+endmodule
